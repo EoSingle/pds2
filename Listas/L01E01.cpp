7c1d39512b3c02fa475cb55c4ad2c833ed12f446
Lucas Albano <olive.albano@gmail.com>
2022-08-26 15:01:22 -0300

    First Commit

---
+++ Listas/L01E01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string matricula = "2022036209";
+    cout << "Hello Student!\n" << matricula << endl;
+    
+    return 0;
+}
